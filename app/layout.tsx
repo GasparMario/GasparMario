e75ddc66e6393a44756fbda61161c879b1237a2b
Mario Gaspar <gaspar.tecinfo@gmail.com>
2026-05-17 21:18:25 -0300

    Atualiza metadata Ellos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "dtf-manager",
-  description: "Sistema web para gerenciamento de PDFs de estampas DTF"
+  title: "Ellos Cadastro",
+  description: "Facilitador de Cadastro de Produtos"
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
